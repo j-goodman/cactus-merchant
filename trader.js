11000e85eaf775b652e136bb2256986a91c963c1
j-goodman <johncurtisgoodman@gmail.com>
2019-02-17 03:22:27 -0500

    Edit text.

--- trader.js
+++ trader.js
@@ -8,7 +8,7 @@ let Trader = function (x, y) {
     this.inventory.fill(null)
     this.inventory[0] = new (wheels.random([Gem, Skull, Gem])) ()
     this.inventory[1] = new (wheels.random([Fruit, Fruit, Fruit, Cactusseed, Skull, Gem])) ()
-    this.info = `A trader named ${this.name}. Traders in the great desert barter with scavengers and tradesmen for survival goods. ${this.name} will buy cactus fruits and other goods off of you or sell you items you might want.`
+    this.info = `A trader named ${this.name}. Traders in the great desert barter with scavengers and tradesmen for survival goods. They'll buy cactus fruits and other goods off of you or sell you items you might want.`
     this.interval = setInterval(this.act.bind(this), 500)
     this.money = Math.floor(Math.random() * 150) + 150
     this.verbs = ['trade']
